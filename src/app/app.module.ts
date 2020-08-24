14eddf271be6754643ec9a0e545211ed8fe28f83
Huy <phamlehuy116@gmail.com>
2020-08-25 00:04:43 +0700

    Motngaybuon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,33 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from 'src/environments/environment'
+import { environment } from 'src/environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/header/login/login.component'
+import {
+  NbSidebarModule,
+  NbSidebarService,
+  NbMenuModule,
+  NbIconModule,
+  NbDialogModule,
+  NbToastrModule,
+  NbToastrService,
+  NbWindowModule
+} from '@nebular/theme';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +41,19 @@ import { environment } from 'src/environments/environment'
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
-    AngularFireStorageModule // storage
+    AngularFireStorageModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule,
+    NbSidebarService,
+    NbMenuModule,
+    NbIconModule,
+    NbDialogModule,
+    NbToastrModule,
+    NbToastrService,
+    NbWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import * as firebase from 'firebase'
+
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { User } from '../models/user.model'
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router) {
+    this.checkCurrentUser();
+  }
+
+  user$: Observable<any>;
+
+  checkCurrentUser() {
+    return this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.db.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+
+  public async logOut() {
+    await this.afAuth.signOut();
+    return this.router.navigate(['/']);
+  }
+
+  updateUserData({ uid, email, displayName, photoURL }: User) {
+    const userRef: AngularFirestoreDocument<User> = this.db.doc(`users/${uid}`);
+    const data = {
+      uid,
+      email,
+      displayName,
+      photoURL
+    };
+    return userRef.set(data, { merge: true });
+  }
+}
