4b3c5b1c10167849ee534c90d4a24a9d6f3fd0c8
Huy <phamlehuy116@gmail.com>
2020-08-24 22:15:00 +0700

    Hettienruiii

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: " AIzaSyCl0NYBWmpA5YengM0yIOM2OkT8POPfGc8 ",
+    authDomain: "spotifyclone-bfc99.firebaseapp.com",
+    databaseURL: "https://spotifyclone-bfc99.firebaseio.com",
+    projectId: "spotifyclone-bfc99",
+    storageBucket: "spotifyclone-bfc99.appspot.com",
+    messagingSenderId: "724812334915 ",
+    appId: " 1: 724812334915: web: 04204e11ec50f602dc51f6 ",
+    measurementId: " G-5959DEQV3Q "
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: " AIzaSyCl0NYBWmpA5YengM0yIOM2OkT8POPfGc8 ",
+    authDomain: "spotifyclone-bfc99.firebaseapp.com",
+    databaseURL: "https://spotifyclone-bfc99.firebaseio.com",
+    projectId: "spotifyclone-bfc99",
+    storageBucket: "spotifyclone-bfc99.appspot.com",
+    messagingSenderId: "724812334915 ",
+    appId: " 1: 724812334915: web: 04204e11ec50f602dc51f6 ",
+    measurementId: " G-5959DEQV3Q "
+  }
 };
 
 /*
