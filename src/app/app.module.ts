1f2fc26757f7c3e93e3145ac6ff47cc65a46b0f5
Huy <phamlehuy116@gmail.com>
2020-08-26 11:12:36 +0700

    Fixxx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,26 +9,14 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import {
-  NbSidebarModule,
-  NbSidebarService,
-  NbMenuModule,
-  NbIconModule,
-  NbDialogModule,
-  NbToastrModule,
-  NbToastrService,
-  NbWindowModule
-} from '@nebular/theme';
-import { MainPageComponent } from './pages/main-page/main-page.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    MainPageComponent
   ],
   imports: [
     BrowserModule,
@@ -38,17 +26,7 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
     AngularFireAuthModule, // auth
     AngularFireStorageModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
-    NbLayoutModule,
-    NbEvaIconsModule,
-    NbSidebarModule,
-    NbSidebarService,
-    NbMenuModule,
-    NbIconModule,
-    NbDialogModule,
-    NbToastrModule,
-    NbToastrService,
-    NbWindowModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
